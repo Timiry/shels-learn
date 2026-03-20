695376cc806e9edb03a192d63570b791abc72fa7
Timiry <irina.v.tim@gmail.com>
2026-03-20 16:55:03 +0300

    add programs api

--- openapi.config.ts
+++ openapi.config.ts
@@ -2,12 +2,23 @@ module.exports = {
   schemaFile: "./api-docs.json", // Путь к вашему OpenAPI JSON
   apiFile: "./src/shared/api/baseApi.ts",
   apiImport: "baseApi",
-  outputFile: "./src/entities/section/model/sectionsApi.ts",
-  exportName: "sectionsApi",
+  outputFile: "./src/features/programs/model/programsApi.ts",
+  exportName: "programsApi",
   hooks: true, // Генерировать хуки (useGetUserQuery и т.д.)
   tag: true, // Автоматические теги для кэширования
   flattenArg: true, // Упростить аргументы запросов
   // Дополнительно:
-  filterEndpoints: ["getById", "update", "delete", "getAll", "create"], // Фильтрация эндпоинтов
+  filterEndpoints: [
+    "getProgram",
+    "updateProgram",
+    "deleteProgram",
+    "getPrograms",
+    "createProgram",
+    "assignGroupsToProgram",
+    "getProgramCourseLists",
+    "updateProgramCourseLists",
+    "getProgramEnrollmentLists",
+    "assignUsersToProgram",
+  ], // Фильтрация эндпоинтов
   // endpointOverrides: [...] // Кастомизация отдельных методов
 };
--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -22,6 +22,7 @@ export const baseApi = createApi({
     "Learning",
     "Review",
     "Sections",
+    "Programs",
   ],
   endpoints: () => ({}),
 });
